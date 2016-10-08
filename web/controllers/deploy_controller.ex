eeab44deacc3f2c0cef675abf5c86a2ce9c9998c
Sergey Novikov <novikov@inventos.ru>
2016-10-08 23:47:06 +0700

    script of deploy

--- web/controllers/deploy_controller.ex
+++ web/controllers/deploy_controller.ex
@@ -2,6 +2,7 @@ defmodule RedButtonServer.DeployController do
   use RedButtonServer.Web, :controller
 
   # TODO: more validations
+  # ./bin/deploy --host=lxc2.int.avs.io --user=aviasales --env=staging --branch=avs350_tarriffs_gates --haproxy-port=8080
   def index(conn, %{"env" => "beta", "branch" => branch}) do
     json conn, %{ok: true, branch: branch}
   end
