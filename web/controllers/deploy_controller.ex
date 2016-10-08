09ccd2bc93cd14e080f44a797e0e246d5d5086c6
Sergey Novikov <novikov@inventos.ru>
2016-10-08 23:46:52 +0700

    simple validation

--- web/controllers/deploy_controller.ex
+++ web/controllers/deploy_controller.ex
@@ -1,9 +1,11 @@
 defmodule RedButtonServer.DeployController do
   use RedButtonServer.Web, :controller
 
-  alias RedButtonServer.Deploy
-
+  # TODO: more validations
+  def index(conn, %{"env" => "beta", "branch" => branch}) do
+    json conn, %{ok: true, branch: branch}
+  end
   def index(conn, _params) do
-    render(conn, "index.json")
+    json conn, %{error: "env and branch are required"}
   end
 end
--- web/views/deploy_view.ex
+++
@@ -1,7 +0,0 @@
-defmodule RedButtonServer.DeployView do
-  use RedButtonServer.Web, :view
-
-  def render("index.json", _) do
-    %{ok: true}
-  end
-end
